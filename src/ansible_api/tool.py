e1885a44b1e27fb7b78cdd50c494a5430db56439
小飞熊 <lfbear@xiongxiaofeideMacBook-Pro.local>
2017-10-10 13:19:43 +0800

    增加错误日志单独记录

--- src/ansible_api/tool.py
+++ src/ansible_api/tool.py
@@ -26,18 +26,23 @@ class Tool(object):
     def init_logger(path):
         log_formatter = "%(asctime)s | %(levelname)s - %(message)s"
         date_formatter = "%Y-%m-%d %H:%M:%S"
+        logFormatter = logging.Formatter(fmt=log_formatter, datefmt=date_formatter)
+        errHandler = logging.handlers.TimedRotatingFileHandler('/var/log/ansible-api.err',when='midnight')
+        errHandler.setFormatter(logFormatter)
+        errHandler.setLevel(logging.WARNING)
         if isinstance(path,str) and os.path.exists(os.path.dirname(path)):
             Tool.LOGGER = logging.getLogger('ansible-api.%s' % __version__)
             logHandler = logging.handlers.TimedRotatingFileHandler(path,when='midnight')
-            logFormatter = logging.Formatter(fmt=log_formatter, datefmt=date_formatter)
             logHandler.setFormatter(logFormatter)
             Tool.LOGGER.addHandler(logHandler)
+            Tool.LOGGER.addHandler(errHandler)
             Tool.LOGGER.setLevel(logging.DEBUG)
             Tool.LOGGER.propagate = False # disable console output
         else:
             logging.basicConfig(level=logging.DEBUG,
                                 format=log_formatter, datefmt=date_formatter)
             Tool.LOGGER = logging.getLogger('ansible-api.%s' % __version__)
+        
 
     @staticmethod
     def getmd5(str):
